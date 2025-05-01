cb00d20b19f2e16cb0b33c4dd005dc3f54c095ba
Phil <phil@pgg-mbp.local>
2025-05-01 11:56:59 -0400

    fix gitignore

--- build.sh
+++ build.sh
@@ -12,5 +12,8 @@ rgbasm -o "$game_dir.o" "$game_dir.asm"
 #Link
 rgblink -o "$game_dir.gb" "$game_dir.o"
 
+#Make symbols for debugging
+rgblink -n "$game_dir.sym" "$game_dir.o"
+
 #Offset ROM for header information
 rgbfix -v -p 0xFF "$game_dir.gb"
